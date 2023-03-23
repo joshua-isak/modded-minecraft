a99666db8773fd94c534446247b7a8c3dfc4989c
Joshua Isak <43322464+joshua-isak@users.noreply.github.com>
2023-03-22 21:37:34 -0400

    Update monitor.lua

--- monitor.lua
+++ monitor.lua
@@ -1,6 +1,7 @@
 print("Initializing Reaction Monitor v1.1")
 
 local component = require("component")
+local term = require("term")
 
 update_interval = 5
 battery = component.big_battery
@@ -9,6 +10,23 @@ reactor = component.br_reactor
 cutoff = component.redstone
 
 bat_capacity = battery.getMaxEnergyStored()
+last_message = ""
+current_row = 1
+
+component.gpu.setResolution(30,30)
+
+function draw_reactor_status(status)
+    string = "Reactor Status: "
+    if (status == true) then
+        string = string .. "Online"
+    else
+        string = string .. "Offline"
+    end
+
+    term.setCursor(1, current_row)
+    term.write(string)
+    current_row = current_row + 1
+end
 
 
 print("starting loop!")
@@ -30,6 +48,9 @@ do
         print("Battery full, deactivating reactor...")
         reactor.setActive(false)
     end
+
+    term.clear()
+    draw_reactor_status(reactor.getActive())
 
 
     os.sleep(update_interval)
